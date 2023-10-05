f038d212dceae93b7172cbd700c140e299acfb1f
kishor jawale <138974502+kishorsama@users.noreply.github.com>
2023-10-05 10:50:38 +0530

    28th septeber 2023
    to calculate momentum from values taken from user of mass and velocity in kg and m/s respectively.
    simple multiplication

---
+++ momentum (2317052).py
@@ -0,0 +1,13 @@
+#This code it to calculate momentum from values taken from user of mass and velocity in kg and m/s respectively.
+
+#input mass in kg
+mass=float(input('Enter mass in kg: '))
+
+#input velocity in m/s
+velocity=float(input('Enter value of velocity in m/s: '))
+
+#calculate momentum as mass*velocity
+momentum=mass*velocity
+
+#output of momentum
+print("momentum is",momentum)
