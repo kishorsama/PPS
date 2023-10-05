e795daaa9de45bbb9566b80513c761da1a791f82
kishor jawale <138974502+kishorsama@users.noreply.github.com>
2023-10-05 10:48:21 +0530

    5th october 2023
    to accept n number from users. Compute and display maximum in list, minimum in list, sum and average of numbers.
    used for loop

---
+++ to accept n number of (2317052).py
@@ -0,0 +1,49 @@
+#to accept n number from users. Compute and display maximum in list, minimum in list, sum and average of numbers.
+
+
+#take total number of input from user
+length=int(input('How many numbers you want to enter? : '))
+
+#make a blank list
+list1=[]
+
+#use for loop in range of length
+for i in range (length):
+    #take each input corresponding to total number of input
+    Number= int(input('Enter a number: '))
+    #add each element to end of list
+    list1.append(Number)
+#just a space
+print(" ")
+
+#calculate summation of entered numbers
+addition=sum(list1)
+
+#calculate max number from entered numbers
+max_num=max(list1)
+
+#calculate min number from entered numbers
+min_num=min(list1)
+
+#calculate average of from entered numbers
+average=max_num/length
+
+#show entered numbers
+print('Entered number are = '),
+print(list1)
+
+#show addition of numbers
+print('Summation of entered number = '),
+print(addition)
+
+#show maximum number
+print("Maximum number from entered number = "),
+print(max_num)
+
+#show minimum number
+print('Minimum number from entered number = '),
+print(min_num)
+
+#show average of numbers
+print('Average of entered number = '),
+print(average)
